39bbb499caf1bd2aaba714f1864a2aff1b18f80a
pyrossh <peter.john@sent.com>
2023-12-22 00:15:21 +0530

    imporve perf

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -9,22 +9,27 @@ const ipInfoClient = new IPinfoWrapper("3f197e26fa9210");
 export async function handler(req: Request, ctx: FreshContext) {
   const resp = await ctx.next();
   const userAgent = req.headers.get("user-agent") || "";
-  const ipRes = await ipInfoClient.lookupIp(ctx.remoteAddr.hostname);
   if (
-    req.headers.get("accept")?.includes("text/html") &&
-    !isbot(userAgent) &&
-    !ipRes.bogon
+    req.headers.get("accept")?.includes("text/html") && !isbot(userAgent)
   ) {
-    const pathname = new URL(req.url).pathname;
-    const referer = req.headers.get("referer") || "";
-    const parser = new UAParser(userAgent);
-    recordVisit(
-      pathname,
-      referer,
-      ipRes.country,
-      parser.getOS().name,
-      parser.getBrowser().name,
-    );
+    ipInfoClient.lookupIp(ctx.remoteAddr.hostname)
+      .then((ipRes: { bogon: boolean; country: string }) => {
+        if (!ipRes.bogon) {
+          const pathname = new URL(req.url).pathname;
+          const referer = req.headers.get("referer") || "";
+          const parser = new UAParser(userAgent);
+          recordVisit(
+            pathname,
+            referer,
+            ipRes.country,
+            parser.getOS().name,
+            parser.getBrowser().name,
+          );
+        }
+      })
+      .catch((err: Error) => {
+        console.log("failed to get ip", err);
+      });
   }
   return resp;
 }
