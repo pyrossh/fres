ce593795e2e96c282d3df9365940fea32ff50f64
pyrossh <peter.john@sent.com>
2024-02-21 16:15:22 +0530

    Fix base path

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
       strict: true
     }),
     paths: {
-      base: process.argv.includes('dev') ? '' : 'pyros.sh'
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
     }
   }
 };
