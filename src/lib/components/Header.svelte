43702e324b7fbace4e3cfa3125790b4bd8463e17
pyrossh <peter.john@sent.com>
2024-04-20 21:03:41 +0530

    fix header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,7 +2,7 @@
 	<nav class="flex w-full flex-1 flex-row justify-center bg-black font-mono text-lg">
 		<div class="flex flex-row flex-1 items-center p-3 max-w-5xl">
 			<a class="flex text-#f1fa8c pr-4 hover:no-underline ml-0 pl-0" href="/">
-				<span class="mr-1 font-logo font-bold">木</span> pyros.sh
+				<span class="mr-1 font-logo font-bold">木</span> pyrossh
 			</a>
 			<div
 				class="flex flex-row flex-1 items-center text-white sm:[&>a]:mx-1 [&>a]:px-2 [&>a:hover]:bg-gray-600 data-current:child:bg-gray-600"
@@ -12,7 +12,7 @@
 				<div>|</div>
 				<a href="/posts" rel="prefetch"> posts </a>
 				<div>|</div>
-				<a href="https://github.com/pyrossh/pyros.sh"> code </a>
+				<a href="https://github.com/pyrossh/pyrossh.dev"> code </a>
 			</div>
 		</div>
 	</nav>
