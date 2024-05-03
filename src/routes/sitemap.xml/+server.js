032ace7e0546a2a30ef17e05f31afb9a3f495b6e
pyrossh <pyrossh>
2024-05-03 22:30:46 +0530

    improve sitemap

---
+++ src/routes/sitemap.xml/+server.js
@@ -0,0 +1,15 @@
+import * as sitemap from 'super-sitemap';
+
+export const prerender = true;
+
+export async function GET() {
+  return await sitemap.response({
+    origin: 'https://pyrossh.dev',
+    paramValues: {
+      '/posts/[slug]': [
+        'eyecandy-golang-error-reporting',
+        'gopibot-to-the-rescue',
+      ]
+    }
+  });
+};
